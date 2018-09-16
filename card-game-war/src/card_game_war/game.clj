e43376605ad543269e2ee938d69d880ac724f9cc
Kay Oh <khtdr.com@gmail.com>
2018-09-16 16:44:23 -0600

    first one!: war game problem

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -1,13 +1,53 @@
 (ns card-game-war.game)
 
-;; feel free to use these cards or use your own data structure
 (def suits [:spade :club :diamond :heart])
 (def ranks [2 3 4 5 6 7 8 9 10 :jack :queen :king :ace])
-(def cards
-  (for [suit suits
-        rank ranks]
-    [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn make-deck []
+  (for [suit suits, rank ranks]
+    {:suit suit :rank rank}))
 
-(defn play-game [player1-cards player2-cards])
+(defn split-rand [coll]
+  (split-at (rand-int (count coll)) coll))
+
+(defn shuffle [deck]
+  (do (loop [shuffled [] unshuffled deck]
+        (if (empty? unshuffled) shuffled
+            (let [[part-1 part-2] (split-rand unshuffled)]
+              (recur (cons (first part-2) shuffled)
+                     (concat part-1 (drop 1 part-2))))))))
+
+(defn deal [deck] (split-at (quot (count deck) 2) deck))
+
+(defn score-card [card]
+  (+ (.indexOf suits (:suit card))
+     (* 4 (.indexOf ranks (:rank card)))))
+
+(defn winner [[card-1 card-2]]
+  (if (> (score-card card-1)
+         (score-card card-2))
+    card-1 card-2))
+
+(defn play-round [[hand-1 hand-2]]
+  (let [card-1 (first hand-1)
+        card-2 (first hand-2)]
+    (if (= card-1 (winner [card-1 card-2]))
+      [(concat (drop 1 hand-1) [(first hand-1) (first hand-2)]) (drop 1 hand-2)]
+      [(drop 1 hand-1) (concat (drop 1 hand-2) [(first hand-2) (first hand-1)])])))
+
+(defn someone-lost? [[hand-1 hand-2]]
+  (or (= 0 (count hand-1))
+      (= 0 (count hand-2))))
+
+(defn end-game [[hand-1 hand-2] round-count]
+  (print "Player" (if (> (count hand-1) (count hand-2)) "1" "2")
+         "wins in" round-count "rounds!"))
+
+(defn play-game [[player-1 player-2]]
+  (do (loop [hand-1 player-1
+             hand-2 player-2
+             round-count 0]
+        (if (someone-lost? [hand-1 hand-2])
+          (end-game [hand-1 hand-2] round-count)
+          (let [[next-hand-1 next-hand-2] (play-round [hand-1 hand-2])]
+            (recur next-hand-1 next-hand-2 (+ 1 round-count)))))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -3,17 +3,67 @@
             [card-game-war.game :refer :all]))
 
 
-;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= {:rank 3 :suit :spade}
+           (winner [{:rank 2 :suit :spade}
+                    {:rank 3 :suit :spade}])
+           )))
+  (testing "order doesn't matter"
+    (is (= {:rank 4 :suit :spade}
+           (winner [{:rank 4 :suit :spade}
+                    {:rank 3 :suit :spade}])
+           )))
+  (testing "jacks are higher rank than numbers"
+    (is (= {:rank :jack :suit :spade}
+           (winner [{:rank 10 :suit :spade}
+                    {:rank :jack :suit :spade}])
+           )))
+  (testing "queens are higher rank than jacks"
+    (is (= {:rank :queen :suit :spade}
+           (winner [{:rank :queen :suit :spade}
+                    {:rank :jack :suit :spade}])
+           )))
+  (testing "kings are higher rank than queens"
+    (is (= {:rank :king :suit :spade}
+           (winner [{:rank :queen :suit :spade}
+                    {:rank :king :suit :spade}])
+           )))
+  (testing "aces are higher rank than kings"
+    (is (= {:rank :ace :suit :spade}
+           (winner [{:rank :king :suit :spade}
+                    {:rank :ace :suit :spade}])
+           )))
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= {:rank :ace :suit :club}
+           (winner [{:rank :ace :suit :club}
+                    {:rank :ace :suit :spade}])
+           )))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= {:rank :ace :suit :diamond}
+           (winner [{:rank :ace :suit :club}
+                    {:rank :ace :suit :diamond}])
+           )))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (= {:rank :ace :suit :heart}
+           (winner [{:rank :ace :suit :heart}
+                    {:rank :ace :suit :diamond}])
+           ))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
-
+  (testing "player 2 has all the high cards, should win every hand"
+    (is (= (with-out-str (play-game (deal (make-deck))))
+           "Player 2 wins in 26 rounds!"
+           )))
+  (testing "player 1 has the low cards and Ace of Hearts, and should win"
+    (is (= (with-out-str (play-game (deal (cons (last (make-deck)) (drop 1 (make-deck))))))
+           "Player 1 wins in 246 rounds!"
+           )))
+  (testing "player 1 wins with ace of hearts against shuffled opponent"
+    (let [deck (make-deck)]
+      (is (= (subs (with-out-str
+                     (play-game (deal (cons (last deck) (drop 1 (shuffle deck)))))
+                     ) 0 13)
+             "Player 1 wins"
+             )
+          ))))
