e14a29f67fa506e8dbfdfbe619bbc8aa4dd2094d
Kay Oh <khtdr.com@gmail.com>
2018-09-20 21:34:24 -0600

    alphabet cipher - v1

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,39 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
+(defn char-idx [ch] (- (int ch) (int \a)))
 
-(defn decode [keyword message]
-  "decodeme")
+(defn encode-char [keyword-char message-char]
+  (.charAt "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+           (+ (char-idx keyword-char) (char-idx message-char))))
 
-(defn decipher [cipher message]
-  "decypherme")
+(defn decode-char [keyword-char cipher-char]
+  (char (+ (int \a) (.indexOf
+                     (subs "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+                           (char-idx keyword-char) (+ 26 (char-idx keyword-char)))
+                     (str cipher-char)))))
 
+(defn transcode [keyword message transcode-char]
+  (loop [i 0 output ""]
+    (if (= i (count message)) output
+      (recur (+ 1 i) (str output
+                          (transcode-char
+                           (.charAt keyword (rem i (count keyword)))
+                           (.charAt message i)))))))
+
+(defn repeats-in? [word text]
+  (cond (empty? text) true
+        (= word text) true
+        (> (count word) (count text)) (= text (subs word 0 (count text)))
+        (= word (subs text 0 (count word))) (repeats-in? word (subs text (count word)))
+        :else false))
+
+(defn find-repeater [text]
+  (do (loop [n 1]
+        (if (>= n (count text)) text
+            (if (repeats-in? (subs text 0 n) text)
+              (subs text 0 n)
+              (recur (+ 1 n)))))))
+
+(defn encode [keyword message] (transcode keyword message encode-char))
+(defn decode [keyword message] (transcode keyword message decode-char))
+(defn decipher [cipher message] (find-repeater (transcode message cipher decode-char)))
